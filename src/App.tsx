922f7a5fbae5482e2baf3b96fe82bd1fa333f729
Anri Tsetskhlashvili <67234362+anritsetskhla10@users.noreply.github.com>
2026-01-05 21:54:25 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold text-yellow-400 mb-8">Pacman</h1>
+      <div className="p-4 border-4 border-blue-600 rounded-lg">
+        <p>Game Board will be here</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
